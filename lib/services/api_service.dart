bf60619cc87f0516a3cfbdc7e3a1249b0ca1c1a7
Eclipse <eclipsedew@proton.me>
2026-09-23 10:22:14 +0800

    Ship: new tunnel URL (trek-diversity-wholesale-roger.trycloudflare.com)

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,7 +9,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ApiService {
   static String get _defaultBaseUrl {
     if (kIsWeb) return 'http://localhost:8080';
-    if (Platform.isAndroid) return 'https://contribution-healthcare-dodge-flights.trycloudflare.com';
+    if (Platform.isAndroid) return 'https://trek-diversity-wholesale-roger.trycloudflare.com';
     return 'http://localhost:8080';
   }
 
